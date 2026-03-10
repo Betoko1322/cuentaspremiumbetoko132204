8267385a1bbd75934cc9aaa9a309b4f84b89a69d
Betoko <albert.zend@gmail.com>
2026-03-10 15:21:38 -0600

    service-worker.js

---
+++ donitas/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE="donitas-cache";
+
+const urls=[
+"./",
+"./index.html",
+"./manifest.json"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>cache.addAll(urls))
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
+});
