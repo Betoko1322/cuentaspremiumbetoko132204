8de01d7d9b6d1b67aa38f6666455c3c0679d09cd
Betoko <albert.zend@gmail.com>
2026-05-13 13:26:49 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "catalogo-streaming-v1";
+const CACHE_NAME = "catalogo-streaming-v3";
 
 const urlsToCache = [
   "./",
@@ -7,20 +7,54 @@ const urlsToCache = [
   "./logocatalogo.jpg"
 ];
 
+// INSTALAR
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+// ACTIVAR
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response => {
+
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+        return response;
+
+      })
+      .catch(() => caches.match(event.request))
+
+  );
+
 });
